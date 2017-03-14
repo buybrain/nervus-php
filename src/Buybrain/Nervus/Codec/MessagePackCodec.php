5ee963f15ec2c9ee642226e812b1c1455e1fce51
Ede Meijer <ede.meijer88@gmail.com>
2017-03-14 11:42:19 +0100

    Deal with missing native MessagePack codec a bit better

--- src/Buybrain/Nervus/Codec/MessagePackCodec.php
+++ src/Buybrain/Nervus/Codec/MessagePackCodec.php
@@ -3,16 +3,15 @@ namespace Buybrain\Nervus\Codec;
 
 class MessagePackCodec implements Codec
 {
-    /** @var bool */
-    private static $nativeSupported;
-
     /**
      * @param resource $stream
      * @return Decoder
      */
     public function newDecoder($stream)
     {
-        return self::nativeSupported() ? new NativeMessagePackDecoder($stream) : new PureMessagePackDecoder($stream);
+        return NativeMessagePackCodec::isSupported() ?
+            new NativeMessagePackDecoder($stream) :
+            new PureMessagePackDecoder($stream);
     }
 
     /**
@@ -21,17 +20,8 @@ class MessagePackCodec implements Codec
      */
     public function newEncoder($stream)
     {
-        return self::nativeSupported() ? new NativeMessagePackEncoder($stream) : new PureMessagePackEncoder($stream);
-    }
-
-    /**
-     * @return bool
-     */
-    private static function nativeSupported()
-    {
-        if (self::$nativeSupported === null) {
-            self::$nativeSupported = extension_loaded('msgpack');
-        }
-        return self::$nativeSupported;
+        return NativeMessagePackCodec::isSupported() ?
+            new NativeMessagePackEncoder($stream) :
+            new PureMessagePackEncoder($stream);
     }
 }
--- src/Buybrain/Nervus/Codec/NativeMessagePackCodec.php
+++ src/Buybrain/Nervus/Codec/NativeMessagePackCodec.php
@@ -1,14 +1,20 @@
 <?php
 namespace Buybrain\Nervus\Codec;
 
+use RuntimeException;
+
 class NativeMessagePackCodec implements Codec
 {
+    /** @var bool */
+    private static $supported;
+    
     /**
      * @param resource $stream
      * @return Decoder
      */
     public function newDecoder($stream)
     {
+        self::assertSupported();
         return new NativeMessagePackDecoder($stream);
     }
 
@@ -18,6 +24,28 @@ class NativeMessagePackCodec implements Codec
      */
     public function newEncoder($stream)
     {
+        self::assertSupported();
         return new NativeMessagePackEncoder($stream);
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isSupported()
+    {
+        if (self::$supported === null) {
+            self::$supported = extension_loaded('msgpack');
+        }
+        return self::$supported;
+    }
+    
+    private static function assertSupported()
+    {
+        if (!self::isSupported()) {
+            throw new RuntimeException(
+                'Native MessagePack codec is not supported. ' .
+                'Please follow the instructions at https://github.com/msgpack/msgpack-php to install it.'
+            );
+        }
     }
 }
